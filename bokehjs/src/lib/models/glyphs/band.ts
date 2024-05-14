hunter7lee/bokeh

Rename {Band,Whisker} glyphs to {Band,Whisker}Glyph

bokehjs/src/lib/models/glyphs/band.ts
@@ -7,11 +7,11 @@ import * as mixins from "core/property_mixins"
 import type * as visuals from "core/visuals"
 import * as p from "core/properties"
 
-export interface BandView extends Band.Data {}
+export interface BandGlyphView extends BandGlyph.Data {}
 
-export class BandView extends GlyphView {
-  declare model: Band
-  declare visuals: Band.Visuals
+export class BandGlyphView extends GlyphView {
+  declare model: BandGlyph
+  declare visuals: BandGlyph.Visuals
 
   protected _index_data(index: SpatialIndex): void {
     const {data_size} = this
@@ -49,14 +49,14 @@ export class BandView extends GlyphView {
     const slower = [this.slower, this.sbase]
     const supper = [this.supper, this.sbase]
 
-    this._define_attr<Band.Data>("lower_sx", slower[i])
-    this._define_attr<Band.Data>("lower_sy", slower[j])
+    this._define_attr<BandGlyph.Data>("lower_sx", slower[i])
+    this._define_attr<BandGlyph.Data>("lower_sy", slower[j])
 
-    this._define_attr<Band.Data>("upper_sx", supper[i])
-    this._define_attr<Band.Data>("upper_sy", supper[j])
+    this._define_attr<BandGlyph.Data>("upper_sx", supper[i])
+    this._define_attr<BandGlyph.Data>("upper_sy", supper[j])
   }
 
-  protected _render(ctx: Context2d, _indices: number[], data?: Band.Data): void {
+  protected _render(ctx: Context2d, _indices: number[], data?: BandGlyph.Data): void {
     const {lower_sx, lower_sy, upper_sx, upper_sy} = {...this, ...data}
 
     const n_lower = lower_sx.length
@@ -96,7 +96,7 @@ export class BandView extends GlyphView {
   }
 }
 
-export namespace Band {
+export namespace BandGlyph {
   export type Attrs = p.AttrsOf<Props>
 
   export type Props = Glyph.Props & {
@@ -125,29 +125,29 @@ export namespace Band {
   }
 }
 
-export interface Band extends Band.Attrs {}
+export interface BandGlyph extends BandGlyph.Attrs {}
 
-export class Band extends Glyph {
-  declare properties: Band.Props
-  declare __view_type__: BandView
+export class BandGlyph extends Glyph {
+  declare properties: BandGlyph.Props
+  declare __view_type__: BandGlyphView
 
-  constructor(attrs?: Partial<Band.Attrs>) {
+  constructor(attrs?: Partial<BandGlyph.Attrs>) {
     super(attrs)
   }
 
   static {
-    this.prototype.default_view = BandView
+    this.prototype.default_view = BandGlyphView
 
-    this.mixins<Band.Mixins>([mixins.LineScalar, mixins.FillScalar, mixins.HatchScalar])
+    this.mixins<BandGlyph.Mixins>([mixins.LineScalar, mixins.FillScalar, mixins.HatchScalar])
 
-    this.define<Band.Props>(() => ({
+    this.define<BandGlyph.Props>(() => ({
       dimension: [ Dimension, "height" ],
       lower:     [ p.XOrYCoordinateSpec, {field: "lower"} ],
       upper:     [ p.XOrYCoordinateSpec, {field: "upper"} ],
       base:      [ p.XOrYCrossCoordinateSpec, {field: "base"} ],
     }))
 
-    this.override<Band.Props>({
+    this.override<BandGlyph.Props>({
       fill_color: "#fff9ba",
       fill_alpha: 0.4,
       line_color: "#cccccc",

bokehjs/src/lib/models/glyphs/whisker.ts
@@ -15,11 +15,11 @@ import * as p from "core/properties"
 
 const {PI} = Math
 
-export interface WhiskerView extends Whisker.Data {}
+export interface WhiskerGlyphView extends WhiskerGlyph.Data {}
 
-export class WhiskerView extends GlyphView {
-  declare model: Whisker
-  declare visuals: Whisker.Visuals
+export class WhiskerGlyphView extends GlyphView {
+  declare model: WhiskerGlyph
+  declare visuals: WhiskerGlyph.Visuals
 
   protected lower_head: ArrowHeadView | null
   protected upper_head: ArrowHeadView | null
@@ -96,14 +96,14 @@ export class WhiskerView extends GlyphView {
     const slower = [this.slower, this.sbase]
     const supper = [this.supper, this.sbase]
 
-    this._define_attr<Whisker.Data>("lower_sx", slower[i])
-    this._define_attr<Whisker.Data>("lower_sy", slower[j])
+    this._define_attr<WhiskerGlyph.Data>("lower_sx", slower[i])
+    this._define_attr<WhiskerGlyph.Data>("lower_sy", slower[j])
 
-    this._define_attr<Whisker.Data>("upper_sx", supper[i])
-    this._define_attr<Whisker.Data>("upper_sy", supper[j])
+    this._define_attr<WhiskerGlyph.Data>("upper_sx", supper[i])
+    this._define_attr<WhiskerGlyph.Data>("upper_sy", supper[j])
   }
 
-  protected _render(ctx: Context2d, indices: number[], data?: Whisker.Data): void {
+  protected _render(ctx: Context2d, indices: number[], data?: WhiskerGlyph.Data): void {
     const {lower_sx, upper_sx, lower_sy, upper_sy} = {...this, ...data}
     const {lower_head, upper_head} = this
     const {dimension} = this.model
@@ -174,7 +174,7 @@ export class WhiskerView extends GlyphView {
   }
 }
 
-export namespace Whisker {
+export namespace WhiskerGlyph {
   export type Attrs = p.AttrsOf<Props>
 
   export type Props = Glyph.Props & {
@@ -198,22 +198,22 @@ export namespace Whisker {
   }
 }
 
-export interface Whisker extends Whisker.Attrs {}
+export interface WhiskerGlyph extends WhiskerGlyph.Attrs {}
 
-export class Whisker extends Glyph {
-  declare properties: Whisker.Props
-  declare __view_type__: WhiskerView
+export class WhiskerGlyph extends Glyph {
+  declare properties: WhiskerGlyph.Props
+  declare __view_type__: WhiskerGlyphView
 
-  constructor(attrs?: Partial<Whisker.Attrs>) {
+  constructor(attrs?: Partial<WhiskerGlyph.Attrs>) {
     super(attrs)
   }
 
   static {
-    this.prototype.default_view = WhiskerView
+    this.prototype.default_view = WhiskerGlyphView
 
-    this.mixins<Whisker.Mixins>(LineVector)
+    this.mixins<WhiskerGlyph.Mixins>(LineVector)
 
-    this.define<Whisker.Props>(({Ref, Nullable}) => ({
+    this.define<WhiskerGlyph.Props>(({Ref, Nullable}) => ({
       dimension:  [ Dimension, "height" ],
       lower:      [ p.XOrYCoordinateSpec, {field: "lower"} ],
       upper:      [ p.XOrYCoordinateSpec, {field: "upper"} ],

swe-train/bokeh__bokeh

bokehjs/src/lib/models/glyphs/band.ts
@@ -7,11 +7,11 @@ import * as mixins from "core/property_mixins"
 import type * as visuals from "core/visuals"
 import * as p from "core/properties"
 
-export interface BandView extends Band.Data {}
+export interface BandGlyphView extends BandGlyph.Data {}
 
-export class BandView extends GlyphView {
-  declare model: Band
-  declare visuals: Band.Visuals
+export class BandGlyphView extends GlyphView {
+  declare model: BandGlyph
+  declare visuals: BandGlyph.Visuals
 
   protected _index_data(index: SpatialIndex): void {
     const {data_size} = this
@@ -49,14 +49,14 @@ export class BandView extends GlyphView {
     const slower = [this.slower, this.sbase]
     const supper = [this.supper, this.sbase]
 
-    this._define_attr<Band.Data>("lower_sx", slower[i])
-    this._define_attr<Band.Data>("lower_sy", slower[j])
+    this._define_attr<BandGlyph.Data>("lower_sx", slower[i])
+    this._define_attr<BandGlyph.Data>("lower_sy", slower[j])
 
-    this._define_attr<Band.Data>("upper_sx", supper[i])
-    this._define_attr<Band.Data>("upper_sy", supper[j])
+    this._define_attr<BandGlyph.Data>("upper_sx", supper[i])
+    this._define_attr<BandGlyph.Data>("upper_sy", supper[j])
   }
 
-  protected _paint(ctx: Context2d, _indices: number[], data?: Band.Data): void {
+  protected _paint(ctx: Context2d, _indices: number[], data?: BandGlyph.Data): void {
     const {lower_sx, lower_sy, upper_sx, upper_sy} = {...this, ...data}
 
     const n_lower = lower_sx.length
@@ -96,7 +96,7 @@ export class BandView extends GlyphView {
   }
 }
 
-export namespace Band {
+export namespace BandGlyph {
   export type Attrs = p.AttrsOf<Props>
 
   export type Props = Glyph.Props & {
@@ -125,29 +125,29 @@ export namespace Band {
   }
 }
 
-export interface Band extends Band.Attrs {}
+export interface BandGlyph extends BandGlyph.Attrs {}
 
-export class Band extends Glyph {
-  declare properties: Band.Props
-  declare __view_type__: BandView
+export class BandGlyph extends Glyph {
+  declare properties: BandGlyph.Props
+  declare __view_type__: BandGlyphView
 
-  constructor(attrs?: Partial<Band.Attrs>) {
+  constructor(attrs?: Partial<BandGlyph.Attrs>) {
     super(attrs)
   }
 
   static {
-    this.prototype.default_view = BandView
+    this.prototype.default_view = BandGlyphView
 
-    this.mixins<Band.Mixins>([mixins.LineScalar, mixins.FillScalar, mixins.HatchScalar])
+    this.mixins<BandGlyph.Mixins>([mixins.LineScalar, mixins.FillScalar, mixins.HatchScalar])
 
-    this.define<Band.Props>(() => ({
+    this.define<BandGlyph.Props>(() => ({
       dimension: [ Dimension, "height" ],
       lower:     [ p.XOrYCoordinateSpec, {field: "lower"} ],
       upper:     [ p.XOrYCoordinateSpec, {field: "upper"} ],
       base:      [ p.XOrYCrossCoordinateSpec, {field: "base"} ],
     }))
 
-    this.override<Band.Props>({
+    this.override<BandGlyph.Props>({
       fill_color: "#fff9ba",
       fill_alpha: 0.4,
       line_color: "#cccccc",

bokehjs/src/lib/models/glyphs/whisker.ts
@@ -15,11 +15,11 @@ import * as p from "core/properties"
 
 const {PI} = Math
 
-export interface WhiskerView extends Whisker.Data {}
+export interface WhiskerGlyphView extends WhiskerGlyph.Data {}
 
-export class WhiskerView extends GlyphView {
-  declare model: Whisker
-  declare visuals: Whisker.Visuals
+export class WhiskerGlyphView extends GlyphView {
+  declare model: WhiskerGlyph
+  declare visuals: WhiskerGlyph.Visuals
 
   protected lower_head: ArrowHeadView | null = null
   protected upper_head: ArrowHeadView | null = null
@@ -96,14 +96,14 @@ export class WhiskerView extends GlyphView {
     const slower = [this.slower, this.sbase]
     const supper = [this.supper, this.sbase]
 
-    this._define_attr<Whisker.Data>("lower_sx", slower[i])
-    this._define_attr<Whisker.Data>("lower_sy", slower[j])
+    this._define_attr<WhiskerGlyph.Data>("lower_sx", slower[i])
+    this._define_attr<WhiskerGlyph.Data>("lower_sy", slower[j])
 
-    this._define_attr<Whisker.Data>("upper_sx", supper[i])
-    this._define_attr<Whisker.Data>("upper_sy", supper[j])
+    this._define_attr<WhiskerGlyph.Data>("upper_sx", supper[i])
+    this._define_attr<WhiskerGlyph.Data>("upper_sy", supper[j])
   }
 
-  protected _paint(ctx: Context2d, indices: number[], data?: Whisker.Data): void {
+  protected _paint(ctx: Context2d, indices: number[], data?: WhiskerGlyph.Data): void {
     const {lower_sx, upper_sx, lower_sy, upper_sy} = {...this, ...data}
     const {lower_head, upper_head} = this
     const {dimension} = this.model
@@ -174,7 +174,7 @@ export class WhiskerView extends GlyphView {
   }
 }
 
-export namespace Whisker {
+export namespace WhiskerGlyph {
   export type Attrs = p.AttrsOf<Props>
 
   export type Props = Glyph.Props & {
@@ -198,22 +198,22 @@ export namespace Whisker {
   }
 }
 
-export interface Whisker extends Whisker.Attrs {}
+export interface WhiskerGlyph extends WhiskerGlyph.Attrs {}
 
-export class Whisker extends Glyph {
-  declare properties: Whisker.Props
-  declare __view_type__: WhiskerView
+export class WhiskerGlyph extends Glyph {
+  declare properties: WhiskerGlyph.Props
+  declare __view_type__: WhiskerGlyphView
 
-  constructor(attrs?: Partial<Whisker.Attrs>) {
+  constructor(attrs?: Partial<WhiskerGlyph.Attrs>) {
     super(attrs)
   }
 
   static {
-    this.prototype.default_view = WhiskerView
+    this.prototype.default_view = WhiskerGlyphView
 
-    this.mixins<Whisker.Mixins>(LineVector)
+    this.mixins<WhiskerGlyph.Mixins>(LineVector)
 
-    this.define<Whisker.Props>(({Ref, Nullable}) => ({
+    this.define<WhiskerGlyph.Props>(({Ref, Nullable}) => ({
       dimension:  [ Dimension, "height" ],
       lower:      [ p.XOrYCoordinateSpec, {field: "lower"} ],
       upper:      [ p.XOrYCoordinateSpec, {field: "upper"} ],